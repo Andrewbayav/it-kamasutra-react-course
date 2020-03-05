b14101b34c1cff7894b0288321cb37068da87192
Andrewbayav <bay.av@yandex.ru>
2020-03-05 09:03:30 +0300

    lesson # 13

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://c7.hotpng.com/preview/631/524/26/logo-company-graphic-designer-royalty-free-design.jpg'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <img src='https://avatars.mds.yandex.net/get-pdb/2264533/76a92258-9d53-467d-861e-e1fd2abc17a2/s1200?webp=false'></img>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                new post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
