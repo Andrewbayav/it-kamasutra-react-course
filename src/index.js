d005934da03fea421b7647b7db2dd776bac830b8
Andrewbayav <bay.av@yandex.ru>
2020-04-06 11:48:28 +0300

    lesson # 45

--- src/index.js
+++ src/index.js
@@ -4,17 +4,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 
-let rerenderEntireTree = (state) => {
-    ReactDOM.render(<App state={state}
-                         dispatch={store.dispatch.bind(store)}
-                         store = {store}/>, document.getElementById('root'));
+let rerenderEntireTree = () => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>,
+        document.getElementById('root'));
 }
 
 rerenderEntireTree(store.getState());
 store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntireTree(state);
+    rerenderEntireTree();
 }); // this is callback!
 
 // If you want your app to work offline and load faster, you can change
