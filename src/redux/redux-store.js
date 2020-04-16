9b4d4b5ed28303ebdaed663ba49625ff8c37834b
Andrewbayav <bay.av@yandex.ru>
2020-04-16 10:13:03 +0300

    lesson # 51

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,12 +2,14 @@ import {combineReducers, createStore} from "redux";
 import {dialogsReducer} from "./dialogs-reducor";
 import {profileReducer} from "./profile-reducor";
 import {sidebarReducer} from "./sidebar-reducor";
+import {usersReducer} from "./users-reducor";
 
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
-    sidebar: sidebarReducer
+    sidebar: sidebarReducer,
+    usersPage: usersReducer
 });
 
 let store = createStore(reducers);
