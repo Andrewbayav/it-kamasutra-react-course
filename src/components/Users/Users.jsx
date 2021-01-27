6e3e167fdfd1d19e44b7a78e97b53f2bcbcd711b
abaykin <abaykin@fork.ru>
2021-01-27 15:44:14 +0300

    lesson 54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,10 +5,6 @@ import userPhoto from './../../assets/images/noava.jpg';
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
-
     componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(responce => {
